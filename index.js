71c43cdf69db5d7dab43e59b8515b8f7d08a64c0
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-03-16 18:04:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
@@ -18,6 +19,31 @@ function johnLennonFacts(facts) {
   }
   return newFacts
 }
+=======
+// const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+// const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+//
+// function theBeatlesPlay(musicians, instruments) {
+//   var array = [];
+//   for (let i = 0; i < 4; i++) {
+//     array.push(`${musicians[i]} plays ${instruments[i]}`)
+//   }
+//   return array;
+// }
+//
+// function johnLennonFacts() {
+//   var facts = ["He was the last Beatle to learn to drive",
+//   "He was never a vegetarian",
+//   "He was a choir boy and boy scout",
+//   "He hated the sound of his own voice"]
+//   var newFacts = []
+//   var i = 0
+//   while (i < facts.length) {
+//     newFacts.push(facts[i] + "!!!")
+//   }
+//   return newFacts
+// }
+>>>>>>> b40de578f4df2d498e5ee0086e22e050b4b4efb6
 
 function iLoveTheBeatles(num) {
   var array = []
